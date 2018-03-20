9d3b6a924a127f342b009711b34302eede64f872
Zhou Kang <dev.zhoukang@gmail.com>
2018-03-20 11:20:37 +0800

    init proj

---
+++ ResponderChain/ResponderChain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResponderChain
+//
+//  Created by Zhou Kang on 2018/3/20.
+//  Copyright © 2018年 Zhou Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResponderChain/ResponderChain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResponderChain
+//
+//  Created by Zhou Kang on 2018/3/20.
+//  Copyright © 2018年 Zhou Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
